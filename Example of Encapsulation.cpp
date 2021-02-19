afadd03f38caed627a0c4ee9251123a2a1a6ce48
seohas <77061677+seohas@users.noreply.github.com>
2021-02-20 04:29:33 +0900

    Update Example of Encapsulation.cpp

--- Example of Encapsulation.cpp
+++ Example of Encapsulation.cpp
@@ -63,3 +63,64 @@ int main(void)
 	return 0;
 }
 //Ring클래스, Cricle 클래스, Point 클래스 모두 자신의 정보를 출력하기 위한 함수를 멤버에 포함시켜서 캡슐화를 완성하였다
+
+//!!Answer with constructor!!
+#include <iostream>
+using namespace std;
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x, int y):xpos(x),ypos(y)
+	{
+		
+	}
+	void ShowPointInfo()const
+	{
+		cout << "[" << xpos << "," << ypos << "]"<<endl;
+	}
+};
+class Circle
+{
+private:
+	int rad;
+	Point center;
+public:
+	Circle(int x, int y, int r):center(x,y)
+	{
+		rad = r;
+	}
+	void ShowCircleInfo() const
+	{
+		cout << "radius: " << rad << endl;
+		center.ShowPointInfo();
+	}
+};
+class Ring
+{
+private:
+	Circle inner;
+	Circle outer;
+public:
+	Ring(int x1, int y1, int r1, int x2, int y2, int r2): inner(x1,y1,r1),outer(x2,y2,r2)
+	{
+
+	}
+	void ShowRingInfo() const
+	{
+		cout << "Inner Circle Info.." << endl;
+		inner.ShowCircleInfo();
+		cout << "outter Circle Info.." << endl;
+		outer.ShowCircleInfo();
+	}
+
+
+};
+int main(void)
+{
+	Ring ring(1, 1, 4, 2, 2, 9);
+	ring.ShowRingInfo();
+
+	return 0;
+}
